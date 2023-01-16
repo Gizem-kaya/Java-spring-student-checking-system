53b8c58bfa8e5433c017dc49c14afe0ad3d31e8f
Gizem <violivaldi@gmail.com>
2023-01-16 13:13:31 +0100

    Database access service is created.

---
+++ src/main/java/com/gizem/demo/student/StudentService.java
@@ -0,0 +1,18 @@
+package com.gizem.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class StudentService {
+
+    public List<Student> getAllStudents () {
+        return List.of(
+                new Student(UUID.randomUUID(), "James", "Bond", "jamesbond@gmail.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(), "Elisa", "Tamara", "elisatamara@gmail.com", Student.Gender.FEMALE)
+        );
+    }
+
+}
